688f8e1ae49826d6a3725c1d7946c5fc5dcfbe23
rafaortega191 <rafaelortega1911@gmail.com>
2023-07-26 01:08:36 -0300

    estructura principal creada

---
+++ index.js
@@ -0,0 +1,7 @@
+//node --watch index.js para activar el que el archivo se vaya actualizando automaticamente
+//si es la primera vez usa (npm install express --save)
+//y usa (npm i babel-cli babel-preset-env express-validator mongoose cors morgan dotenv)
+//instalar esto (npm install -g nodemon)
+import Express  from "express";
+
+console.log("prueba");
